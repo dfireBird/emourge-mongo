08b789a4ceb69b15373f36f8143dad6ca1d9d67f
dfireBird <me@dfirebird.codes>
2021-01-03 22:47:56 +0530

    feat: implement reactionRemove listener to decrease frequency condition: frequency is decreased when a reaction is removed

---
+++ src/listeners/ReactionRemoveListener.ts
@@ -0,0 +1,39 @@
+import { Listener } from "discord-akairo";
+import { MessageReaction } from "discord.js";
+import { guildModel, IEmojiFrequency } from "../models/guildModel";
+
+export default class ReactionRemoveListener extends Listener {
+    constructor() {
+        super("reactionRm", {
+            emitter: "client",
+            event: "messageReactionRemove",
+        });
+    }
+
+    public async exec(reaction: MessageReaction) {
+        const msg = reaction.message;
+        const result = reaction.emoji.identifier.match(/:\d+/g);
+        const emojiId = result !== null ? result[0].slice(1) : "";
+
+        if (msg.guild === null) return;
+        const emojiManager = msg.guild.emojis;
+        const emoji = emojiManager.resolve(emojiId);
+
+        if (emoji === null) return;
+        const guild = await guildModel.findOne({ id: msg.guild.id }).exec();
+
+        if (guild === null) return;
+        const index = guild.emojiFrequency.findIndex((e) => {
+            if (emoji === null) return;
+            return e.emojiId == emoji.id;
+        });
+
+        guild.emojiFrequency[index].frequency--;
+
+        try {
+            await guild.save();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
